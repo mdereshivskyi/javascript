46ee79f2fce5c79743c72539d98647bc4902ea16
mdereshivskyi <m.dereshivskyi@gmail.com>
2024-01-18 17:43:02 +0200

    lesson

---
+++ js/index.js
@@ -0,0 +1,23 @@
+// lesson1
+
+// console.log("How are you?");
+// console.error("How are you?");
+// console.warn("How are you?");
+// console.info("How are you?");
+
+//lesson2
+
+var num = 5;
+
+console.log(num);
+
+console.log("Result: " + num + "!");
+
+const number = 10;
+
+console.log(number);
+
+// number = 5;
+// console.log(number);
+
+var some_new = 
